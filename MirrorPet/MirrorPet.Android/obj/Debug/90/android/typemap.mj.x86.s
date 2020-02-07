fc7167d4edadf2bbde7600aea07be9d840f04486
herayang <47582117+herayang@users.noreply.github.com>
2020-02-07 03:15:30 -0700

    New_File
    New file to hopefully fix an issue

---
+++ MirrorPet/MirrorPet.Android/obj/Debug/90/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 8d3dd2bfd236067fd925c10540e5908063da562a */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1491
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 390643
+	.include	"typemap.mj.inc"
