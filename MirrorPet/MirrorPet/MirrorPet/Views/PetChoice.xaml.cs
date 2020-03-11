13de0a4912ce05296d11f62948d28668462c51cb
ChaosMartel <g8sofchaos@gmail.com>
2020-03-11 15:14:10 -0600

    Added the Pet Choice page
    Added the base page where you will set your goal and choose your pet. Just uploading it so it is there. No fuctionality yet.

---
+++ MirrorPet/MirrorPet/MirrorPet/Views/PetChoice.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace mirrorPet.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PetChoice : ContentPage
+    {
+        public PetChoice()
+        {
+            InitializeComponent();
+        }
+    }
+}
